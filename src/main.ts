420df6103e1c58cb6b674b1c6c68fbb553591ef1
Huespal <Huespal@users.noreply.github.com>
2025-06-25 16:58:54 +0200

    feat: add CSRF protection

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import cookieParser from 'cookie-parser';
+import { doubleCsrf } from 'csrf-csrf';
 import helmet from 'helmet';
 import { join } from 'path';
 import { AppModule } from './app.module';
@@ -9,9 +11,18 @@ async function bootstrap() {
   const isProd = process.env.MODE === 'production';
 
   const app = await NestFactory
-    .create<NestExpressApplication>(AppModule, { cors: true });
+    .create<NestExpressApplication>(AppModule, { cors: !isProd });
 
-  app.use(helmet());
+  app.use(helmet({
+    crossOriginEmbedderPolicy: isProd,
+    crossOriginResourcePolicy: isProd
+  }));
+  app.use(cookieParser());
+  const { doubleCsrfProtection } = doubleCsrf({
+    getSecret: (_req) => 'bookstore',
+    getSessionIdentifier: (req) => req.ip ?? ''
+  });
+  app.use(doubleCsrfProtection);
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.useGlobalPipes(new ValidationPipe({ disableErrorMessages: isProd }));
 
