f2a6b9eef8fd86d70e8821f4304e69bf543ffc0b
Huespal <Huespal@users.noreply.github.com>
2025-06-25 16:07:20 +0200

    feat: add Helmet

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import helmet from 'helmet';
 import { join } from 'path';
 import { AppModule } from './app.module';
 
@@ -10,6 +11,7 @@ async function bootstrap() {
   const app = await NestFactory
     .create<NestExpressApplication>(AppModule, { cors: true });
 
+  app.use(helmet());
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.useGlobalPipes(new ValidationPipe({ disableErrorMessages: isProd }));
 
